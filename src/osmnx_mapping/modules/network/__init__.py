4b9c3f40b67b64f133ad0bf7a795afb57c469149
Provost Simon <simon1.provost@epitech.eu>
2025-02-28 10:04:37 -0500

    feat(modules): add mapping chaining support w/ new network factory

--- src/osmnx_mapping/modules/network/__init__.py
+++ src/osmnx_mapping/modules/network/__init__.py
@@ -1,7 +1,9 @@
 from .abc_network import NetworkBase
 from .networks import OSMNxNetwork
+from .network_factory import NetworkFactory as CreateNetwork
 
 __all__ = [
     "NetworkBase",
     "OSMNxNetwork",
+    "CreateNetwork",
 ]
--- src/osmnx_mapping/modules/network/helpers/check_output_column.py
+++ src/osmnx_mapping/modules/network/helpers/check_output_column.py
@@ -1,5 +1,6 @@
 from typing import Callable
 import geopandas as gpd
+import pandas as pd
 from beartype import beartype
 
 
@@ -23,6 +24,14 @@ def check_output_column(
             raise ValueError(
                 f"GeoDataFrame already contains column '{output_column}'. "
                 "Please update the parameter 'output_column'."
+            )
+        if not pd.api.types.is_numeric_dtype(data[latitude_column]):
+            data[latitude_column] = pd.to_numeric(
+                data[latitude_column], errors="coerce"
+            )
+        if not pd.api.types.is_numeric_dtype(data[longitude_column]):
+            data[longitude_column] = pd.to_numeric(
+                data[longitude_column], errors="coerce"
             )
         return function_to_wrap(
             self,
