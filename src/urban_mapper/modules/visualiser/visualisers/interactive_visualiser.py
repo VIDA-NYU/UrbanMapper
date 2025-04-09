521a33e77fd9edab7339d979fba2a30954277b31
Provost Simon <simon1.provost@epitech.eu>
2025-04-09 11:29:38 -0400

    refactor(modules): add interactive vis' colorbar label text color support
    Thanks to @fabiofelix for showing this out more than one time.

--- src/urban_mapper/modules/visualiser/visualisers/interactive_visualiser.py
+++ src/urban_mapper/modules/visualiser/visualisers/interactive_visualiser.py
@@ -4,6 +4,10 @@ import ipywidgets as widgets
 from IPython.display import display
 from urban_mapper.modules.visualiser.abc_visualiser import VisualiserBase
 from beartype import beartype
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import branca.colormap as cm
 
 
 @beartype
@@ -26,6 +30,7 @@ class InteractiveVisualiser(VisualiserBase):
         "vmax",
         "width",
         "height",
+        "colorbar_text_color",
     }
 
     def _render(
@@ -35,8 +40,39 @@ class InteractiveVisualiser(VisualiserBase):
             raise ValueError("At least one column must be specified.")
         render_kwargs = {**self.style, **kwargs}
 
+        legend = render_kwargs.pop("legend", True)
+        text_color = render_kwargs.pop("colorbar_text_color", "black")
+
         def get_map(column):
-            return urban_layer_geodataframe.explore(column=column, **render_kwargs)
+            if pd.api.types.is_numeric_dtype(urban_layer_geodataframe[column]):
+                vmin = render_kwargs.get("vmin", urban_layer_geodataframe[column].min())
+                vmax = render_kwargs.get("vmax", urban_layer_geodataframe[column].max())
+                cmap = render_kwargs.get("cmap", "viridis")
+
+                folium_map = urban_layer_geodataframe.explore(
+                    column=column, legend=False, **render_kwargs
+                )
+
+                if legend:
+                    mpl_cmap = plt.get_cmap(cmap)
+
+                    colors = mpl_cmap(np.linspace(0, 1, 256))
+                    colors = [tuple(color) for color in colors]
+                    colormap = cm.LinearColormap(
+                        colors=colors,
+                        vmin=vmin,
+                        vmax=vmax,
+                        caption=column,
+                        text_color=text_color,
+                    )
+
+                    folium_map.add_child(colormap)
+            else:
+                folium_map = urban_layer_geodataframe.explore(
+                    column=column, legend=legend, **render_kwargs
+                )
+
+            return folium_map
 
         if len(columns) == 1:
             return get_map(columns[0])
@@ -60,8 +96,6 @@ class InteractiveVisualiser(VisualiserBase):
         if format == "ascii":
             return "Visualiser: InteractiveVisualiser using Folium"
         elif format == "json":
-            return {
-                "visualiser": "InteractiveVisualiser using Folium",
-            }
+            return {"visualiser": "InteractiveVisualiser using Folium"}
         else:
             raise ValueError(f"Unsupported format '{format}'")
